ebc438cc931d3feac52049e06b6c0d1da9f68c7a
justyamzal <justyamzal@gmail.com>
2025-02-23 08:53:49 +0700

    add code.js

---
+++ fcc-js/code.js
@@ -0,0 +1,31 @@
+// todo 
+// ?
+// *
+// !
+
+
+// console.log("Hello World!!");
+
+
+
+//--- STARTED ---//
+
+// let
+// let message = "Hello";
+// message = "World";
+// console.log(message);
+
+//const
+// const christmast_2025 = '12.25.2025';
+// christmast_2025 = '12.26.2026';
+// console.log(chrismast_2025);
+
+// ---
+// note
+
+//--- PRIMITIVE DATA TYPES ---//
+
+// String
+// let favoriteFruit = "Strawberries";
+// let favoriteIceCream = "Chocolate";
+// let favoriteProgrammingLanguange = "Javascript";
