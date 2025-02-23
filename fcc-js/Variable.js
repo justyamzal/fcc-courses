d3395fc1cd88d5bdba0a9a6d0d65aa987a3d392b
justyamzal <justyamzal@gmail.com>
2025-02-23 09:19:09 +0700

    renaming file

--- fcc-js/Variable.js
+++ fcc-js/Variable.js
@@ -10,22 +10,46 @@
 
 //--- STARTED ---//
 
-// let
+// * let : defines data and can be changed and reassign later
 // let message = "Hello";
 // message = "World";
 // console.log(message);
 
-//const
+// * const : defines data and cannot be changed 
 // const christmast_2025 = '12.25.2025';
 // christmast_2025 = '12.26.2026';
 // console.log(chrismast_2025);
 
-// ---
-// note
+ 
 
 //--- PRIMITIVE DATA TYPES ---//
 
-// String
+// * String
 // let favoriteFruit = "Strawberries";
 // let favoriteIceCream = "Chocolate";
 // let favoriteProgrammingLanguange = "Javascript";
+// console.log(favoriteFruit,favoriteIceCream,favoriteProgrammingLanguange);
+
+// * Number
+
+console.log();
+
+
+
+
+
+// * Big Int 
+console.log();
+
+// * Undefined
+console.log();
+
+// * Null
+console.log();
+
+// * Symbol
+console.log();
+
+
+
+// * Object
