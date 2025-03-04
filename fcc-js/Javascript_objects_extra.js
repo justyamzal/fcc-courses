9eeea77059baf5937194c00770ee4510ae6078e3
justyamzal <justyamzal@gmail.com>
2025-03-04 07:53:53 +0700

    adding extra file

---
+++ fcc-js/Javascript_objects_extra.js
@@ -0,0 +1,6 @@
+  // ----- OBJECT LITERALS -----/
+  /* 
+  Object Literals adalah cara paling sederhana untuk membuat objek di JavaScript. Objek adalah kumpulan properti (key-value pairs) yang dapat menyimpan data (nilai) dan fungsi (method). Objek literal dibuat menggunakan sintaks {} (kurung kurawal).
+  */
+
+  
