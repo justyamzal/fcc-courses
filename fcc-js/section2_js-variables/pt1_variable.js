de71b45b80e32f7b6f461a37be21f596acd4e1cb
justyamzal <justyamzal@gmail.com>
2025-03-15 09:57:07 +0700

    adding sec 2

---
+++ fcc-js/section2_js-variables/pt1_variable.js
@@ -0,0 +1,12 @@
+//--- STARTED ---//
+// console.log("Hello World!!");
+
+// * let : defines data and can be changed and reassign later
+// let message = "Hello";
+// message = "World";
+// console.log(message);
+
+// * const : defines data and cannot be changed 
+// const christmast_2025 = '12.25.2025';
+// christmast_2025 = '12.26.2026';
+// console.log(chrismast_2025);
